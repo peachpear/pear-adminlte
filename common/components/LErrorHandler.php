a36951cd277a54a8c5dde570604eb9b295404575
zhengjunyuan <zhengjunyuan@peilian.com>
2019-01-28 14:24:09 +0800

    异常处理修改

--- common/components/LErrorHandler.php
+++ common/components/LErrorHandler.php
@@ -15,28 +15,19 @@ use yii\web\HttpException;
 class LErrorHandler extends ErrorHandler
 {
     /**
-     * 渲染异常
-     * @param \Error|\Exception $exception
-     */
-    public function renderException($exception)
-    {
-        $this->handleException($exception);
-    }
-
-    /**
      * 异常处理
      * $data['code'] ? $data['code'] : 500
      * @param $exception
      */
-	public function handleException($exception)
-	{
-	    if (YII_DEBUG) {
-	        ini_set("display_errors", true);
+    public function handleException($exception)
+    {
+        if (YII_DEBUG) {
+            ini_set("display_errors", true);
 
-	        throw $exception;
+            throw $exception;
         }
 
-		$data = $this->formatException($exception);
+        $data = $this->formatException($exception);
 
         /** @var $app Application */
         $app = Yii::$app;
@@ -55,13 +46,13 @@ class LErrorHandler extends ErrorHandler
             YII_DEBUG ? $data['message'] : array(),
             YII_DEBUG ? $data : array()
         );
-	}
+    }
 
     /**
      * 错误处理
      */
-	public function handleError($code, $message, $file, $line)
-	{
+    public function handleError($code, $message, $file, $line)
+    {
         /** @var $app Application */
         $app = Yii::$app;
         /** @var $controller LController */
@@ -87,53 +78,12 @@ class LErrorHandler extends ErrorHandler
             YII_DEBUG ? $message : array(),
             YII_DEBUG ? $data : array()
         );
-	}
-
-    /**
-     * 格式化异常
-     * @param $exception
-     * @return array
-     */
-	protected function formatException($exception)
-	{
-        $fileName = $exception->getFile();
-        $errorLine = $exception->getLine();
-
-		$trace = $exception->getTrace();
-
-		foreach ($trace as $i => $t)
-		{
-			if (!isset($t['file'])) {
-				$trace[$i]['file'] = 'unknown';
-			}
-
-			if (!isset($t['line'])) {
-				$trace[$i]['line'] = 0;
-			}
-
-			if (!isset($t['function'])) {
-				$trace[$i]['function'] = 'unknown';
-			}
-
-			unset($trace[$i]['object']);
-		}
-
-		return array(
-			'code' => ($exception instanceof HttpException) ? $exception->statusCode : 500,
-			'type' => get_class($exception),
-			'errorCode' => $exception->getCode(),
-			'message' => $exception->getMessage(),
-			'file' => $fileName,
-			'line' => $errorLine,
-			'trace' => $exception->getTraceAsString(),
-//			'traces' => $trace,
-		);
-	}
+    }
 
     /**
      * 致命错误处理
      */
-	public function handleFatalError()
+    public function handleFatalError()
     {
         $error = error_get_last();
         if (LException::isFatalError($error)) {
@@ -149,5 +99,57 @@ class LErrorHandler extends ErrorHandler
 
             $this->handleException($exception);
         }
+    }
+
+    /**
+     * 格式化异常
+     * @param $exception
+     * @return array
+     */
+    protected function formatException($exception)
+    {
+        $fileName = $exception->getFile();
+        $errorLine = $exception->getLine();
+
+        $trace = $exception->getTrace();
+
+        foreach ($trace as $i => $t)
+        {
+            if (!isset($t['file'])) {
+                $trace[$i]['file'] = 'unknown';
+            }
+
+            if (!isset($t['line'])) {
+                $trace[$i]['line'] = 0;
+            }
+
+            if (!isset($t['function'])) {
+                $trace[$i]['function'] = 'unknown';
+            }
+
+            unset($trace[$i]['object']);
+        }
+
+        return array(
+            'code' => ($exception instanceof HttpException) ? $exception->statusCode : 500,
+            'type' => get_class($exception),
+            'errorCode' => $exception->getCode(),
+            'message' => $exception->getMessage(),
+            'file' => $fileName,
+            'line' => $errorLine,
+            'trace' => $exception->getTraceAsString(),
+//			'traces' => $trace,
+        );
+    }
+
+    /**
+     * 渲染异常输出
+     * 追踪父类可知，这里并不会用到
+     * @param \Error|\Exception $exception
+     */
+    public function renderException($exception)
+    {
+        // 本类上面异常处理、程序退出处理其中一个方法删了，这里可用到
+        $this->handleException($exception);
     }
 }
--- console/components/LConsoleErrorHandler.php
+++ console/components/LConsoleErrorHandler.php
@@ -27,29 +27,6 @@ class LConsoleErrorHandler extends ErrorHandler
         }
     }
 
-    /**
-     * 发邮件
-     * @param $data
-     * @internal param $exception
-     */
-    public function sendErrorMsg( $data )
-    {
-        /** @var LRabbitQueue $queue */
-        $queue = Yii::$app->get("queue");
-        $params = [
-            'send_to' => $this->sendTo,
-            'cc_to' => $this->sendCC,
-            'text' => json_encode( $data ),
-            'title' => "[".ENV.']cli-exception-error',
-            'file' => []
-        ];
-        $queue->produce(
-            $params,
-            'async',
-            'mail'
-        );
-    }
-
     public function handleError($code, $message, $file, $line)
     {
         $exception =  new \ErrorException($message, $code, 1, $file, $line);
@@ -57,6 +34,29 @@ class LConsoleErrorHandler extends ErrorHandler
         $this->sendErrorMsg($this->formatException($exception));
     }
 
+    public function handleFatalError()
+    {
+        $error = error_get_last();
+        if (LException::isFatalError($error)) {
+            $exception = new \ErrorException($error['message'], 500, $error['type'], $error['file'], $error['line']);
+            $this->exception = $exception;
+
+//            $this->logException($exception);
+
+            if ($this->discardExistingOutput) {
+                $this->clearOutput();
+            }
+            // need to explicitly flush logs because exit() next will terminate the app immediately
+            Yii::getLogger()->flush(true);
+            $this->handleException($exception);
+        }
+    }
+
+    /**
+     * 准备邮件中异常/错误的格式
+     * @param $exception
+     * @return array
+     */
     protected function formatException($exception)
     {
         $fileName = $exception->getFile();
@@ -92,25 +92,33 @@ class LConsoleErrorHandler extends ErrorHandler
         );
     }
 
-    public function renderException($exception)
+    /**
+     * 发邮件
+     * @param $data
+     * @internal param $exception
+     */
+    public function sendErrorMsg( $data )
     {
-        $this->handleException($exception);
+        /** @var LRabbitQueue $queue */
+        $queue = Yii::$app->get("queue");
+        $params = [
+            'send_to' => $this->sendTo,
+            'cc_to' => $this->sendCC,
+            'text' => json_encode( $data ),
+            'title' => "[".ENV.']cli-exception-error',
+            'file' => []
+        ];
+        $queue->produce($params, 'async', 'mail');
     }
 
-    public function handleFatalError()
+    /**
+     * 渲染异常输出
+     * 追踪父类可知，这里并不会用到
+     * @param \Exception $exception
+     */
+    public function renderException($exception)
     {
-        $error = error_get_last();
-        if (LException::isFatalError($error)) {
-            $exception = new \ErrorException($error['message'], 500, $error['type'], $error['file'], $error['line']);
-            $this->exception = $exception;
-//            $this->logException($exception);
-
-            if ($this->discardExistingOutput) {
-                $this->clearOutput();
-            }
-            // need to explicitly flush logs because exit() next will terminate the app immediately
-            Yii::getLogger()->flush(true);
-            $this->handleException($exception);
-        }
+        // 本类上面异常处理、程序退出处理其中一个方法删了，这里可用到
+        $this->handleException($exception);
     }
 }
